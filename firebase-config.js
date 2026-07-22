08ed5eca447d39badbc54360708bda86d4822ef5
nopqr shovon <nopqrshov337@gmail.com>
2026-07-22 12:10:57 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,48 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  addDoc,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+  serverTimestamp,
+  arrayUnion,
+  arrayRemove,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB15-BRCf0ejIYCDb4Wx-N70gXGx9-R29I",
+  authDomain: "simple-store-4175f.firebaseapp.com",
+  projectId: "simple-store-4175f",
+  storageBucket: "simple-store-4175f.firebasestorage.app",
+  messagingSenderId: "708569270600",
+  appId: "1:708569270600:web:c5e647d52de75fdf194b7d",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db, 
+  signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, 
+  onAuthStateChanged, sendPasswordResetEmail,
+  collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, addDoc,
+  query, where, orderBy, onSnapshot, serverTimestamp, arrayUnion, arrayRemove
+};
